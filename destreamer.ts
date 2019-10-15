01808fb301787c1f69ad749fc63f70880c6fceb0
snobu <foo@snobu.org>
2019-10-15 07:39:19 +0300

    Move tests to new file

--- destreamer.ts
+++ destreamer.ts
@@ -3,8 +3,7 @@ import puppeteer from 'puppeteer';
 import { terminal as term } from 'terminal-kit';
 import fs from 'fs';
 import path from 'path';
-import { AssertionError } from 'assert';
-
+import { BrowserTests } from './BrowserTests';
 
 // Type in your username here (the one you use to
 // login to Microsoft Stream).
@@ -43,25 +42,6 @@ function sanityChecks() {
             'Example: npm start https://www.microsoftstream.com/video/6f1a382b-e20c-44c0-98fc-5608286e48bc\n');
         process.exit(-1);
     }
-}
-
-async function runBrowserTest() {
-    console.log('[BROWSER TEST] Launching headless Chrome...');
-    const browser = await puppeteer.launch({
-        // Switch to false if you need to login interactively
-        headless: true,
-        args: ['--disable-dev-shm-usage']
-    });
-    const page = await browser.newPage();
-    await page.goto("https://github.com/", { waitUntil: 'networkidle2' });
-    let pageTitle = await page.title();
-    await browser.close();
-    if (!pageTitle.includes('GitHub'))
-    {
-        console.log('[BROWSER TEST] FAIL: Page title does not include "GitHub"');
-        process.exit(44);
-    }
-    console.log('[BROWSER TEST] PASS');
 }
 
 async function rentVideoForLater() {
@@ -149,7 +129,7 @@ async function exfiltrateCookie(page: puppeteer.Page) {
 // We should probably use Mocha or something
 if (args[0] === 'test')
 {
-    runBrowserTest();
+    BrowserTests();
 }
 
 else {
