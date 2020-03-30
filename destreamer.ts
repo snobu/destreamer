8827618296e692207b51fcd4bc41ff56224fbe33
snobu <foo@snobu.org>
2020-03-30 11:32:16 +0300

    Fix lowercase aliases for parameters

--- destreamer.ts
+++ destreamer.ts
@@ -17,9 +17,9 @@ import axios from 'axios';
  */
 
 const argv = yargs.options({
-    videoUrls: { type: 'array', demandOption: true },
+    videoUrls: { type: 'array', alias: 'videourls', demandOption: true },
     username: { type: 'string', demandOption: false },
-    outputDirectory: { type: 'string', default: 'videos' },
+    outputDirectory: { type: 'string', alias: 'outputdirectory', default: 'videos' },
     format: {
         alias:"f",
         describe: `Expose youtube-dl --format option, for details see\n
