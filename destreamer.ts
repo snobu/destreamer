079825a5b59eb90ba76797f7ce99ca8199e159b1
Luca <lukaarma@gmail.com>
2020-04-02 11:31:23 +0200

    Changed verbose error loggin Now it should properly log all the error

--- destreamer.ts
+++ destreamer.ts
@@ -220,7 +220,8 @@ async function getVideoInfo(videoID: string, session: any) {
             console.error(error.response.data);
             console.error("Exiting...");
             if (argv.verbose) {
-                console.error(`[VERBOSE] ${error}`);
+                term.red("[VERBOSE]");
+                console.error(error)
             }
             process.exit(29);
         });
