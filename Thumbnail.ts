388ec0c9a3d62c145ad5e04d2fde78886bdb2cf7
snobu <foo@snobu.org>
2020-04-08 12:06:26 +0300

    Fix line breaks for ffmpeg cmd

--- Thumbnail.ts
+++ Thumbnail.ts
@@ -3,9 +3,9 @@ import { execSync } from 'child_process';
 
 
 export function drawThumbnail(posterImage: string, accessToken: string): void {
-    let fetchCmd = `ffmpeg -hide_banner -loglevel warning ` +
-        `-headers "Authorization: Bearer ${accessToken}\r\n" ` +
-        `-i "${posterImage}" -y .thumbnail.png`;
+    let fetchCmd = `ffmpeg -hide_banner -loglevel warning \
+        -headers "Authorization: Bearer ${accessToken}" \
+        -i "${posterImage}" -y .thumbnail.png`;
     execSync(fetchCmd, { stdio: 'inherit' });
     try {
         term.drawImage('.thumbnail.png', { shrink: { width: 50, height: 50 } });
