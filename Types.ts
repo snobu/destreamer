a2a4bb5f0074cd258bb067ae76dc0c2958bb61b7
Luca <lukaarma@gmail.com>
2020-04-10 15:38:56 +0200

    Created an error struct with the code as key

--- Types.ts
+++ Types.ts
@@ -4,8 +4,22 @@ export type Session = {
     ApiGatewayVersion: string;
 }
 
+
 export type Metadata = {
     title: string;
     playbackUrl: string;
     posterImage: string;
+}
+
+
+interface Errors {
+    [key: number]: string
+}
+
+export const Errors: Errors = {
+    22: 'ffmpeg not found in $PATH',
+    25: 'cannot split videoID from videUrl',
+    27: 'no hlsUrl in the API response',
+    29: 'invalid response from API',
+    88: 'error extracting cookies'
 }
