b3fd6544cc9c83c5c64dc50ce45699463458b6a1
Luca <lukaarma@gmail.com>
2020-04-10 12:59:09 +0200

    changed function name to be more significant

--- destreamer.ts
+++ destreamer.ts
@@ -1,4 +1,4 @@
-import { sleep, getVideoUrls, checkRequirements } from './utils';
+import { sleep, parseVideoUrls, checkRequirements } from './utils';
 import { TokenCache } from './TokenCache';
 import { getVideoMetadata } from './Metadata';
 import { Metadata, Session } from './Types';
@@ -78,6 +78,7 @@ function init() {
 }
 
 async function DoInteractiveLogin(username?: string): Promise<Session> {
+
     console.log('Launching headless Chrome to perform the OpenID Connect dance...');
     const browser = await puppeteer.launch({
         headless: false,
@@ -230,7 +231,7 @@ async function main() {
         process.exit(-1);
     }
 
-    videoUrls = getVideoUrls(argv.videoUrls);
+    videoUrls = parseVideoUrls(argv.videoUrls);
     if (videoUrls.length === 0) {
         console.error(colors.red('\nERROR: No valid URL has been found!\n'));
         process.exit(-1);
