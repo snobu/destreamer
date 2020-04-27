fdcffc800158205f7366c46f66ad67f31386052a
skings <samanf74@gmail.com>
2020-04-27 19:50:57 +0200

    Change readme

--- destreamer.ps1
+++ destreamer.ps1
@@ -1,1 +1,8 @@
-node.exe build\src\destreamer.js $args
+$NodeVersion = Invoke-Expression "node.exe --version"
+if ($NodeVersion.StartsWith("v8.")) {
+    node.exe build\src\destreamer.js $args
+}
+else {
+    node.exe --max-http-header-size 32768 build\src\destreamer.js $args
+}
+
--- destreamer.sh
+++ destreamer.sh
@@ -1,1 +1,8 @@
-node build/src/destreamer.js "$@"
+#!/usr/bin/env bash
+NODE_VERSION=$(node --version)
+
+if [[ $NODE_VERSION == "v8."* ]]; then
+    node build/src/destreamer.js "$@"
+else
+    node --max-http-header-size 32768 build/src/destreamer.js "$@"
+fi
