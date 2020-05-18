4938fb63707255bbe0ba575e5e30a517d0e86235
snobu <foo@snobu.org>
2020-05-18 20:23:12 +0300

    Make tests faster maybe

--- test/test.ts
+++ test/test.ts
@@ -11,14 +11,14 @@ describe('Puppeteer', () => {
     it('should grab GitHub page title', async () => {
         browser = await puppeteer.launch({
             headless: true,
-            args: ['--disable-dev-shm-usage']
+            args: ['--disable-dev-shm-usage', '--fast-start', '--no-sandbox']
         });
         page = await browser.newPage();
         await page.goto("https://github.com/", { waitUntil: 'load' });
         let pageTitle = await page.title();
         assert.equal(true, pageTitle.includes('GitHub'));
         await browser.close();
-    }).timeout(15000); // yeah, this may take a while...
+    }).timeout(25000); // yeah, this may take a while...
 });
 
 describe('Destreamer', () => {
