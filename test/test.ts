f1b0c2abf968b0a7b0c765e28333d1d3c0880d76
Luca <lukaarma@gmail.com>
2020-06-30 23:36:26 +0200

    updated test to reflect GUIDs output after parsing

--- test/test.ts
+++ test/test.ts
@@ -44,12 +44,12 @@ describe('Destreamer parsing', () => {
             'https://web.microsoftstream.com/video/xxxxxx-gggg-xxxx-xxxx-xxxxxxxxxxxx',
             ''
         ];
-        const expectedUrlOut: Array<string> = [
-            'https://web.microsoftstream.com/video/xxxxxxxx-aaaa-xxxx-xxxx-xxxxxxxxxxxx',
-            'https://web.microsoftstream.com/video/xxxxxxxx-bbbb-xxxx-xxxx-xxxxxxxxxxxx',
-            'https://web.microsoftstream.com/video/xxxxxxxx-cccc-xxxx-xxxx-xxxxxxxxxxxx',
-            'https://web.microsoftstream.com/video/xxxxxxxx-dddd-xxxx-xxxx-xxxxxxxxxxxx',
-            'https://web.microsoftstream.com/video/xxxxxxxx-eeee-xxxx-xxxx-xxxxxxxxxxxx'
+        const expectedGUIDsOut: Array<string> = [
+            'xxxxxxxx-aaaa-xxxx-xxxx-xxxxxxxxxxxx',
+            'xxxxxxxx-bbbb-xxxx-xxxx-xxxxxxxxxxxx',
+            'xxxxxxxx-cccc-xxxx-xxxx-xxxxxxxxxxxx',
+            'xxxxxxxx-dddd-xxxx-xxxx-xxxxxxxxxxxx',
+            'xxxxxxxx-eeee-xxxx-xxxx-xxxxxxxxxxxx'
         ];
         const expectedDirOut: Array<string> = [
             'videos',
@@ -61,15 +61,15 @@ describe('Destreamer parsing', () => {
         const tmpFile = tmp.fileSync({ postfix: '.txt' });
         fs.writeFileSync(tmpFile.fd, testIn.join('\r\n'));
         const [testUrlOut , testDirOut]: Array<Array<string>> = await parseInputFile(tmpFile.name, 'videos', testSession);
-        if (testUrlOut.length !== expectedUrlOut.length) {
+        if (testUrlOut.length !== expectedGUIDsOut.length) {
             throw "Expected url list and test list don't have the same number of elements".red;
         }
         else if (testDirOut.length !== expectedDirOut.length) {
             throw "Expected dir list and test list don't have the same number of elements".red;
         }
-        assert.deepStrictEqual(testUrlOut, expectedUrlOut,
+        assert.deepStrictEqual(testUrlOut, expectedGUIDsOut,
             'Error in parsing the URLs, missmatch between test and expected'.red);
-        assert.deepStrictEqual(testUrlOut, expectedUrlOut,
+        assert.deepStrictEqual(testUrlOut, expectedGUIDsOut,
             'Error in parsing the DIRs, missmatch between test and expected'.red);
         assert.ok('Parsing of input file ok');
     });
