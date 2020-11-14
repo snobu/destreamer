e9070511cfa8573ca7a09faa697b559291586cd3
snobu <foo@snobu.org>
2020-11-14 20:08:45 +0200

    Srsly really fixed group download this time

--- src/Utils.ts
+++ src/Utils.ts
@@ -20,8 +20,8 @@ async function extractGuids(url: string, client: ApiClient): Promise<Array<strin
         return [videoMatch[1]];
     }
     else if (groupMatch) {
-        const videoNumber: number = await client.callApi(`groups/${groupMatch[1]}`, 'get')
-            .then((response: AxiosResponse<any> | undefined) => response?.data.metrics.videos);
+        // const videoNumber: number = await client.callApi(`groups/${groupMatch[1]}`, 'get')
+        //     .then((response: AxiosResponse<any> | undefined) => response?.data.metrics.videos);
 
         // Anything over $top=100 will return a 400 Bad Request
         let result: Array<string> = await client.callApi(`groups/${groupMatch[1]}/videos?$top=100&$orderby=publishedDate asc`, 'get')
